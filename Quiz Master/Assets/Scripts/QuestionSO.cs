104b0b5f1d9b4d6a6fe5848fab2955c9e8f6fbb3
John McGee <mcgee336@morrisville.edu>
2023-10-11 12:57:21 -0400

    Scriptable Objects

---
+++ Quiz Master/Assets/Scripts/QuestionSO.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
+public class QuestionSO : ScriptableObject
+{
+    [TextArea(2,6)]
+    [SerializeField] string strquestion = "Enter new question here";  
+}
