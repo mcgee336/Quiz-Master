c2cce6820ed98fd9fe37225fc46e2b32d8f2734e
John McGee <mcgee336@morrisville.edu>
2023-10-11 13:14:13 -0400

    Arrays

--- Quiz Master/Assets/Scripts/QuestionSO.cs
+++ Quiz Master/Assets/Scripts/QuestionSO.cs
@@ -9,9 +9,20 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2,6)]
     [SerializeField] string strquestion = "Enter new question here";
-
+    [SerializeField] string[] stranswers = new string[4];
+    [SerializeField] int intCorrectAnswerIndex;
     public string GetQuestion()
     {
         return strquestion;
     }
+    public string GetAnswer(int index)
+    {
+        return stranswers[index];
+    }
+    public int GetCorrectAnserIndex()
+    {
+        return intCorrectAnswerIndex;
+    }
+
+
 }
